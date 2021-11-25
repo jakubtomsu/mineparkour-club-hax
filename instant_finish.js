f4144843c41ffd5f515ad14011a7c6b6eed13f1d
Jakub Tomšů <66876057+kubatomsu@users.noreply.github.com>
2021-11-25 15:49:42 +0100

    Create instant_finish.js

---
+++ instant_finish.js
@@ -0,0 +1,3 @@
+controls.getObject().position.x = (end.min.x + end.max.x) / 2
+controls.getObject().position.y = (end.min.y + end.max.y) / 2
+controls.getObject().position.z = (end.min.z + end.max.z) / 2
